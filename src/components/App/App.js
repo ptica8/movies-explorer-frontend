dc7afd799bd3b97321c708c64ea68508fa0965ac
Valeria Mikshina <lera-mikshina@mail.ru>
2022-10-21 12:41:26 +0200

    change header for movies route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,19 +2,36 @@ import './App.css';
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Main from "../Main/Main";
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes, Link, useLocation } from 'react-router-dom';
+import icon from '../../images/icon-profile.svg';
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
         <Routes>
             <Route path="/" element={
                 <>
-                    <Header>
+                    <Header location={location.pathname}>
                         <Link to="/" className="header__btn header__btn-registration">Регистрация</Link>
                         <Link to="/" className="header__btn header__btn-login">Войти</Link>
                     </Header>
                     <Main />
+                    <Footer />
+                </>
+            }/>
+            <Route path="/movies" element={
+                <>
+                    <Header location={location.pathname}>
+                        <Link to="/movies" className="header__btn header__btn-films">Фильмы</Link>
+                        <Link to="/saved-movies" className="header__btn header__btn-saved">Сохраненные фильмы</Link>
+                        <Link to="/profile" className="header__btn header__btn-profile">
+                            Аккаунт
+                            <img src={icon} className="header__icon-profile"/>
+                        </Link>
+                    </Header>
+
                     <Footer />
                 </>
             }/>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 
-export default function Header({children}) {
+export default function Header({location, children}) {
     return (
-        <header className="header">
+        <header className={`header ${location === '/' ? 'header__theme_color' : 'header__theme_dark'}`}>
             <Link className="header__logo" to="/" />
             <div className="header__navigation">{children}</div>
         </header>
